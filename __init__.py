fc6190423bdb38a08d6bfd68bac214ba4b8b7cc9
almostserious <mail@marcus-peters.de>
2019-05-18 18:38:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+
 import logging
 import voluptuous as vol
 
@@ -6,22 +7,23 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (
     CONF_PASSWORD, CONF_USERNAME)
 
-_VERSION = '1.1.1'
+_VERSION = '1.1.0'
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'spotcast'
 CONF_DEVICE_NAME = 'device_name'
+CONF_ENTITY_ID = 'entity_id'
 CONF_SPOTIFY_URI = 'uri'
 CONF_ACCOUNTS = 'accounts'
 CONF_SPOTIFY_ACCOUNT = 'account'
-CONF_TRANSFER_PLAYBACK = 'transfer_playback'
 
 SERVICE_START_COMMAND_SCHEMA = vol.Schema({
-    vol.Required(CONF_DEVICE_NAME): cv.string,
-    vol.Optional(CONF_SPOTIFY_URI): cv.string,
-    vol.Optional(CONF_SPOTIFY_ACCOUNT): cv.string,
-    vol.Optional(CONF_TRANSFER_PLAYBACK): cv.boolean
+    vol.Optional(CONF_DEVICE_NAME): cv.string,
+    vol.Optional(CONF_ENTITY_ID): cv.string,
+    vol.Required(CONF_SPOTIFY_URI): cv.string,
+    vol.Optional(CONF_SPOTIFY_ACCOUNT): cv.string
+
 })
 
 ACCOUNTS_SCHEMA = vol.Schema({
@@ -44,9 +46,10 @@ def setup(hass, config):
     username = conf[CONF_USERNAME]
     password = conf[CONF_PASSWORD]
     accounts = conf.get(CONF_ACCOUNTS)
-
+	
     # sensor
     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+
 
     # service
     def get_chromcast_device(device_name):
@@ -70,17 +73,13 @@ def setup(hass, config):
         return access_token, expires
 
     def play(client, spotify_device_id, uri):
-        _LOGGER.info('Got uri: %s', uri)
+        _LOGGER.debug('Playing URI: %s on device-id: %s', uri, spotify_device_id)
         if uri.find('track') > 0:
-            _LOGGER.info('Playing track using uris= for uri: %s', uri)
+            _LOGGER.debug('Playing track using uris= for uri: %s', uri)
             client.start_playback(device_id=spotify_device_id, uris=[uri])
         else:
-            _LOGGER.info('Playing context uri using context_uri for uri: %s', uri)
+            _LOGGER.debug('Playing context uri using context_uri for uri: %s', uri)
             client.start_playback(device_id=spotify_device_id, context_uri=uri)
-    
-    def transfer_pb(client, spotify_device_id):
-        _LOGGER.info('Transfering playback.')
-        client.transfer_playback(device_id=spotify_device_id, force_play = True)
 
     def start_casting(call):
         """service called."""
@@ -89,10 +88,12 @@ def setup(hass, config):
         import spotipy
 
         uri = call.data.get(CONF_SPOTIFY_URI)
-        device_name = call.data.get(CONF_DEVICE_NAME)
-        transfer_playback = call.data.get(CONF_TRANSFER_PLAYBACK)
-        _LOGGER.info('Transfer playback: %s', transfer_playback)
-        _LOGGER.info('Starting spotify on %s', device_name)
+        if call.data.get(CONF_DEVICE_NAME) is None:
+            device_name = hass.states.get(call.data.get(CONF_ENTITY_ID)).attributes.get('friendly_name')
+        else:
+            device_name = call.data.get(CONF_DEVICE_NAME)
+
+        _LOGGER.debug('Starting spotify on %s', device_name)
 
         # Find chromecast device
         cast = get_chromcast_device(device_name)
@@ -102,12 +103,12 @@ def setup(hass, config):
         user = username
         pwd = password
         if account is not None:
-            _LOGGER.info('setting up with different account than default %s', account)
+            _LOGGER.debug('setting up with different account than default %s', account)
             user = accounts.get(account).get(CONF_USERNAME)
             pwd = accounts.get(account).get(CONF_PASSWORD)
 
         # login as real browser to get powerful token
-        access_token, expires = get_spotify_token(username=username, password=password)
+        access_token, expires = get_spotify_token(username=user, password=pwd)
 
         client = spotipy.Spotify(auth=access_token)
 
@@ -117,30 +118,25 @@ def setup(hass, config):
         sp.launch_app()
 
         if not sp.is_launched and not sp.credential_error:
-            _LOGGER.info('Failed to launch spotify controller due to timeout')
-
+            raise HomeAssistantError('Failed to launch spotify controller due to timeout')
         if not sp.is_launched and sp.credential_error:
-            _LOGGER.info('Failed to launch spotify controller due to credential error')
+            raise HomeAssistantError('Failed to launch spotify controller due to credentials error')
 
         spotify_device_id = None
         devices_available = client.devices()
-
         for device in devices_available['devices']:
             if device['id'] == sp.device:
                 spotify_device_id = device['id']
                 break
 
         if not spotify_device_id:
-            _LOGGER.info('No device with id "{}" known by Spotify'.format(sp.device))
-            _LOGGER.info('Known devices: {}'.format(devices_available['devices']))
+            _LOGGER.error('No device with id "{}" known by Spotify'.format(sp.device))
+            _LOGGER.error('Known devices: {}'.format(devices_available['devices']))
+            return
 
-        if transfer_playback == True:
-            transfer_pb(client, spotify_device_id)
-        else:
-            play(client, spotify_device_id, uri)
+        play(client, spotify_device_id, uri)
 
     hass.services.register(DOMAIN, 'start', start_casting,
                            schema=SERVICE_START_COMMAND_SCHEMA)
 
     return True
-
