9055368feac3269b23cb290ddfd3122b64540562
Niklas Fondberg <niklas.fondberg@gmail.com>
2019-09-22 12:20:53 +0200

    v2.2

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -7,7 +7,7 @@ from homeassistant.components.cast.media_player import KNOWN_CHROMECAST_INFO_KEY
 import random
 import time
 
-_VERSION = '2.0.0'
+_VERSION = '2.2.0'
 DOMAIN = 'spotcast'
 
 _LOGGER = logging.getLogger(__name__)
