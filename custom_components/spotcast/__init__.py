27a751a8075db10ca8f1c221b5ca5982ae9abc5b
rileymd88 <riley.macdonald@gmail.com>
2019-10-13 20:28:37 +0200

    added repeat option

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -19,6 +19,7 @@ CONF_ACCOUNTS = 'accounts'
 CONF_SPOTIFY_ACCOUNT = 'account'
 CONF_TRANSFER_PLAYBACK = 'transfer_playback'
 CONF_RANDOM = 'random_song'
+CONF_REPEAT = 'repeat'
 
 SERVICE_START_COMMAND_SCHEMA = vol.Schema({
     vol.Optional(CONF_DEVICE_NAME): cv.string,
@@ -26,7 +27,8 @@ SERVICE_START_COMMAND_SCHEMA = vol.Schema({
     vol.Optional(CONF_SPOTIFY_URI): cv.string,
     vol.Optional(CONF_SPOTIFY_ACCOUNT): cv.string,
     vol.Optional(CONF_TRANSFER_PLAYBACK): cv.boolean,
-    vol.Optional(CONF_RANDOM): cv.boolean
+    vol.Optional(CONF_RANDOM): cv.boolean,
+    vol.Optional(CONF_REPEAT): cv.string
 })
 
 ACCOUNTS_SCHEMA = vol.Schema({
@@ -86,7 +88,7 @@ def setup(hass, config):
         expires = data[1] - int(time.time())
         return access_token, expires
 
-    def play(client, spotify_device_id, uri, random_song):
+    def play(client, spotify_device_id, uri, random_song, repeat):
         # import spotipy
         # import http.client as http_client
         # spotipy.trace = True
@@ -112,6 +114,10 @@ def setup(hass, config):
 
             _LOGGER.debug('Playing context uri using context_uri for uri: "%s" (random_song: %s)', uri, random_song)
             client.start_playback(**kwargs)
+        if repeat:
+            _LOGGER.debug('Turning repeat on')
+            time.sleep(5)
+            client.repeat(state=repeat, device_id=spotify_device_id)
 
     def transfer_pb(client, spotify_device_id):
         _LOGGER.debug('Transfering playback')
@@ -126,6 +132,7 @@ def setup(hass, config):
 
         uri = call.data.get(CONF_SPOTIFY_URI)
         random_song = call.data.get(CONF_RANDOM, False)
+        repeat = call.data.get(CONF_REPEAT)
 
         # Get device name from tiehr device_name or entity_id
         device_name = None
@@ -196,7 +203,7 @@ def setup(hass, config):
         if transfer_playback == True:
             transfer_pb(client, spotify_device_id)
         else:
-            play(client, spotify_device_id, uri, random_song)
+            play(client, spotify_device_id, uri, random_song, repeat)
 
     hass.services.register(DOMAIN, 'start', start_casting,
                            schema=SERVICE_START_COMMAND_SCHEMA)
