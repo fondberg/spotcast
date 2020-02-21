01c6cd84830218713c3fa2c0124610e560dacba2
kleinc80 <59744911+kleinc80@users.noreply.github.com>
2020-02-21 12:20:02 +0100

    @kleinc80 improvements
    Thanks @kleinc80 for this!
    
    
    * Fix for issue 58 (random song not working for albums)
    
    * Improved fix for issue 58 (random_sond won't work for albums)
    
    * Added options shuffle and offset
    
    * Small improvement to random_song logic in conjunction with offset
    parameter
    
    * - Added check for spotify devices
    - Added option to provide spotify id directly
    - Transfer playback improved
    
    * Minor corrections

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -10,11 +10,12 @@ from homeassistant.components.cast.media_player import KNOWN_CHROMECAST_INFO_KEY
 import random
 import time
 
-_VERSION = '2.7.2'
+_VERSION = '2.8.0'
 DOMAIN = 'spotcast'
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_SPOTIFY_DEVICE_ID = 'spotify_device_id'
 CONF_DEVICE_NAME = 'device_name'
 CONF_ENTITY_ID = 'entity_id'
 CONF_SPOTIFY_URI = 'uri'
@@ -23,6 +24,8 @@ CONF_SPOTIFY_ACCOUNT = 'account'
 CONF_TRANSFER_PLAYBACK = 'transfer_playback'
 CONF_RANDOM = 'random_song'
 CONF_REPEAT = 'repeat'
+CONF_SHUFFLE = 'shuffle'
+CONF_OFFSET = 'offset'
 
 WS_TYPE_SPOTCAST_PLAYLISTS = "spotcast/playlists"
 
@@ -30,15 +33,17 @@ SCHEMA_PLAYLISTS = websocket_api.BASE_COMMAND_MESSAGE_SCHEMA.extend(
     {vol.Required("type"): WS_TYPE_SPOTCAST_PLAYLISTS}
 )
 
-
 SERVICE_START_COMMAND_SCHEMA = vol.Schema({
     vol.Optional(CONF_DEVICE_NAME): cv.string,
+    vol.Optional(CONF_SPOTIFY_DEVICE_ID): cv.string,
     vol.Optional(CONF_ENTITY_ID): cv.string,
     vol.Optional(CONF_SPOTIFY_URI): cv.string,
     vol.Optional(CONF_SPOTIFY_ACCOUNT): cv.string,
-    vol.Optional(CONF_TRANSFER_PLAYBACK): cv.boolean,
-    vol.Optional(CONF_RANDOM): cv.boolean,
-    vol.Optional(CONF_REPEAT): cv.string
+    vol.Optional(CONF_TRANSFER_PLAYBACK, default=False): cv.boolean,
+    vol.Optional(CONF_RANDOM, default=False): cv.boolean,
+    vol.Optional(CONF_REPEAT, default='off'): cv.string,
+    vol.Optional(CONF_SHUFFLE, default=False): cv.boolean,
+    vol.Optional(CONF_OFFSET, default=0): cv.string
 })
 
 ACCOUNTS_SCHEMA = vol.Schema({
@@ -83,7 +88,7 @@ async def async_setup(hass, config):
         expires = data[1] - int(time.time())
         return access_token, expires
 
-    def play(client, spotify_device_id, uri, random_song, repeat):
+    def play(client, spotify_device_id, uri, random_song, repeat, shuffle, position):
         # import spotipy
         # import http.client as http_client
         # spotipy.trace = True
@@ -102,17 +107,26 @@ async def async_setup(hass, config):
                 uri = playlists['items'][random.randint(0, no_playlists - 1)]['uri']
             kwargs = {'device_id': spotify_device_id, 'context_uri': uri}
             if random_song:
-                results = client.user_playlist_tracks("me", uri)
-                position = random.randint(0, results['total'] - 1)
+                if uri.find('album') > 0:
+                    results = client.album_tracks(uri)
+                    position = random.randint(0, results['total'] - 1)
+                elif uri.find('playlist') > 0:
+                    results = client.playlist_tracks(uri)
+                    position = random.randint(0, results['total'] - 1)
                 _LOGGER.debug('Start playback at random position: %s', position)
-                kwargs['offset'] = {'position': position}
-
+            kwargs['offset'] = {'position': position}
             _LOGGER.debug('Playing context uri using context_uri for uri: "%s" (random_song: %s)', uri, random_song)
             client.start_playback(**kwargs)
-        if repeat:
-            _LOGGER.debug('Turning repeat on')
-            time.sleep(5)
-            client.repeat(state=repeat, device_id=spotify_device_id)
+        if shuffle or repeat:
+            time.sleep(3)
+            if shuffle:
+                _LOGGER.debug('Turning shuffle on')
+                time.sleep(2)
+                client.shuffle(state=shuffle, device_id=spotify_device_id)
+            if repeat:
+                _LOGGER.debug('Turning repeat on')
+                time.sleep(2)
+                client.repeat(state=repeat, device_id=spotify_device_id)
 
     def get_account_credentials(call):
         """ Get credentials for account """
@@ -129,11 +143,15 @@ async def async_setup(hass, config):
         """ Check if something is playing """
         uri = call.data.get(CONF_SPOTIFY_URI)
         if uri is None or uri.strip() == '' or call.data.get(CONF_TRANSFER_PLAYBACK):
-            current_playback = client.current_playback()
-            if current_playback is not None:
-                _LOGGER.debug('current_playback from spotipy: %s', current_playback)
-                return True
+            return True
         return False
+
+    def getSpotifyConnectDeviceId(client, device_name):
+        devices_available = client.devices()
+        for device in devices_available['devices']:
+            if device['name'] == device_name:
+                return device['id']
+        return None
 
     async def start_casting(call):
         """service called."""
@@ -142,6 +160,9 @@ async def async_setup(hass, config):
         uri = call.data.get(CONF_SPOTIFY_URI)
         random_song = call.data.get(CONF_RANDOM, False)
         repeat = call.data.get(CONF_REPEAT)
+        shuffle = call.data.get(CONF_SHUFFLE)
+        spotify_device_id = call.data.get(CONF_SPOTIFY_DEVICE_ID)
+        position = call.data.get(CONF_OFFSET)
 
         # Account
         user, pwd = get_account_credentials(call)
@@ -152,18 +173,26 @@ async def async_setup(hass, config):
         # get the spotify web api client
         client = spotipy.Spotify(auth=access_token)
 
-        # launch the app on chromecast
-        spotify_cast_device = SpotifyCastDevice(hass, call.data.get(CONF_DEVICE_NAME), call.data.get(CONF_ENTITY_ID))
-        spotify_cast_device.startSpotifyController(access_token, expires)
-        spotify_device_id = spotify_cast_device.getSpotifyDeviceId(client)
+        # first, rely on spotify id given in config
+        if not spotify_device_id:
+            # if not present, check if there's a spotify connect device with that name
+            spotify_device_id = getSpotifyConnectDeviceId(client, call.data.get(CONF_DEVICE_NAME))
+        if not spotify_device_id:
+            # if still no id available, check cast devices and launch the app on chromecast
+            spotify_cast_device = SpotifyCastDevice(hass, call.data.get(CONF_DEVICE_NAME), call.data.get(CONF_ENTITY_ID))
+            spotify_cast_device.startSpotifyController(access_token, expires)
+            spotify_device_id = spotify_cast_device.getSpotifyDeviceId(client)
 
         transfer_playback = shouldTransferPlayback(call, client)
         if transfer_playback == True:
             _LOGGER.debug('Transfering playback')
-            client.transfer_playback(
-                device_id=spotify_device_id, force_play=True)
+            current_playback = client.current_playback()
+            if current_playback is not None:
+                client.transfer_playback(device_id=spotify_device_id, force_play=True)
+            else:
+                client.transfer_playback(device_id=spotify_device_id, force_play=False)
         else:
-            play(client, spotify_device_id, uri, random_song, repeat)
+            play(client, spotify_device_id, uri, random_song, repeat, shuffle, position)
 
     # Register websocket and service
     hass.components.websocket_api.async_register_command(
