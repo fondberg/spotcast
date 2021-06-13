7d9929ad33e9659a1c629bbc4d0094486f03f3c3
wannesmarynen <wannesmarynen@users.noreply.github.com>
2021-06-13 22:56:23 +0200

    fixed None issue (#228)
    * fixed issue
    
    fixed the none issue and cleared up parameters
    
    * Update spotcast_controller.py

--- custom_components/spotcast/spotcast_controller.py
+++ custom_components/spotcast/spotcast_controller.py
@@ -154,10 +154,10 @@ class SpotcastController:
     accounts: dict = {}
     hass = None
 
-    def __init__(self, hass, sp_dc, sp_key, accounts):
-        if accounts:
-            self.accounts = accounts
-        accounts["default"] = OrderedDict([("sp_dc", sp_dc), ("sp_key", sp_key)])
+    def __init__(self, hass, sp_dc, sp_key, accs):
+        if accs:
+            self.accounts = accs
+        self.accounts["default"] = OrderedDict([("sp_dc", sp_dc), ("sp_key", sp_key)])
         self.hass = hass
 
     def get_token_instance(self, account=None):
