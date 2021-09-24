f98f912bf54b4adab3bc5869459d96221df617ac
Felix Cusson <60357894+Darkfull-Dante@users.noreply.github.com>
2021-09-24 13:18:23 -0400

    added method validates status of spotify core

--- custom_components/spotcast/helpers.py
+++ custom_components/spotcast/helpers.py
@@ -36,6 +36,19 @@ def get_spotify_devices(hass, spotify_user_id):
         _LOGGER.debug("get_spotify_devices: %s", resp)
         return resp
 
+def get_spotify_install_status(hass):
+
+    platform_string = "spotify"
+    platforms = entity_platform.async_get_platforms(hass, platform_string)
+    platform_count = len(platforms)
+
+    if platform_count == 0:
+        _LOGGER.error("%s integration not found", platform_string)
+    else:
+        _LOGGER.debug("%s integration found", platform_string)
+
+    return platform_count != 0
+
 
 def get_cast_devices(hass):
     platforms = entity_platform.async_get_platforms(hass, "cast")
