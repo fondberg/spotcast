89f2f9e61d94bf0f234144a1534d1118ab4435e5
Erik <erik@montnemery.com>
2022-02-28 09:01:58 +0100

    Adapt cast platform to changes in HA Core 2022.3

--- custom_components/spotcast/cast.py
+++ custom_components/spotcast/cast.py
@@ -16,18 +16,9 @@ async def async_get_media_browser_root_object(
     cast_type: str
 ) -> list[BrowseMedia]:
     """Create a root object for media browsing."""
-    _LOGGER.debug("async_get_media_browser_root_object")
-    return [
-        BrowseMedia(
-            title="Spotify",
-            media_class=MEDIA_CLASS_APP,
-            media_content_id="",
-            media_content_type="spotify",
-            thumbnail="https://brands.home-assistant.io/_/spotify/logo.png",
-            can_play=False,
-            can_expand=True,
-        )
-    ]
+    result = await ha_spotify.async_browse_media(hass, None, None)
+    _LOGGER.debug("async_get_media_browser_root_object return %s", result.children)
+    return result.children
 
 
 async def async_browse_media(
@@ -48,11 +39,6 @@ async def async_browse_media(
         # Browse deeper in the tree
         result = await ha_spotify.async_browse_media(
             hass, media_content_type, media_content_id, can_play_artist=False
-        )
-    if media_content_type == "spotify":
-        # Browse the root node created by async_get_media_browser_root_object
-        result = await ha_spotify.async_browse_media(
-            hass, None, None, can_play_artist=False
         )
     _LOGGER.debug(
         "async_browse_media return: %s",
@@ -77,7 +63,9 @@ async def async_play_media(
     # If this is a spotify URI, forward to the the spotcast.start service, if not return
     # False
     if media_id and media_id.startswith("spotify:"):
-        data = {"entity_id": cast_entity_id, "uri": media_id}
+        # Get the spotify URI
+        spotify_uri = ha_spotify.spotify_uri_from_media_browser_url(media_id)
+        data = {"entity_id": cast_entity_id, "uri": spotify_uri}
         await hass.services.async_call("spotcast", "start", data, blocking=False)
         return True
     return False
