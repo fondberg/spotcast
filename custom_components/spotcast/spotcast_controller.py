43574fdaaa7c2cf782a1801e2d80fca355494f86
Hmmbob <33529490+hmmbob@users.noreply.github.com>
2022-09-21 11:43:56 +0200

    Tell people to update their `sp_dc` and `sp_key` on token error

--- custom_components/spotcast/spotcast_controller.py
+++ custom_components/spotcast/spotcast_controller.py
@@ -161,7 +161,8 @@ class SpotifyToken:
             _LOGGER.error("Could not get spotify token. sp_dc and sp_key could be expired. Please update in config.")
             raise HomeAssistantError("Expired sp_dc, sp_key")
         except (TokenError, Exception):  # noqa: E722
-            raise HomeAssistantError("Could not get spotify token.")
+            _LOGGER.error("Could not get spotify token. Update your sp_dc and sp_key and try again after a restart of Home Assistant.")
+            raise HomeAssistantError("Could not get spotify token. Update your sp_dc and sp_key and try again after a restart of Home Assistant.")
 
     async def start_session(self):
         """ Starts session to get access token. """
