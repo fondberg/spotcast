11d32e6d9cd060239ee9e34e93a35b5967ede833
Felix Cusson <felixcusson@gmail.com>
2024-08-29 09:46:31 -0400

    gitignore config folder

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -1,18 +1,20 @@
-from test import hello_world
 from logging import getLogger
+
+from homeassistant.core import HomeAssistant
+from homeassistant.util.yaml.objects import NodeDictClass
+
+from custom_components.spotcast.test import hello_world
 
 DOMAIN = "spotcast"
 LOGGER = getLogger(__name__)
 
 
-def setup(hass, config) -> bool:
+def setup(hass: HomeAssistant, config: NodeDictClass) -> bool:
     """Initial setup of spotcast
 
     Returns:
         - bool: returns `True` if the integration setup was successfull
     """
-    LOGGER.error(type(hass))
-    LOGGER.error(type(config))
     LOGGER.error(hello_world())
 
     return True
