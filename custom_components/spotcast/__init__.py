6179750854baf6166aa7d61ca2ea7d51eb1bf03a
Felix Cusson <felixcusson@gmail.com>
2024-11-20 21:05:04 -0500

    Bump version: 4.0.0-a0 → 5.0.0-a0

--- custom_components/spotcast/__init__.py
+++ custom_components/spotcast/__init__.py
@@ -25,7 +25,7 @@ from custom_components.spotcast.config_flow.option_flow_handler import (
     DEFAULT_OPTIONS
 )
 
-__version__ = "4.0.0-a0"
+__version__ = "5.0.0-a0"
 
 
 LOGGER = getLogger(__name__)
